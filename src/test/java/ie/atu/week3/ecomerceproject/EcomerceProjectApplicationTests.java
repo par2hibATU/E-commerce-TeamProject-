9e8bd9e0046ae13685516c6192efeeefccce19e1
par2hibATU <parthibbdcox@gmail.com>
2024-11-07 14:17:03 +0000

    First Commit with database Connection

---
+++ src/test/java/ie/atu/week3/ecomerceproject/EcomerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week3.ecomerceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerceProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
